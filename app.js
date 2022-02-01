e64b1a8913996c1d28ac39bc350503355cef7329
Anna Kohler <a.kohler.capa@gmail.com>
2022-02-01 11:25:52 -0500

    fixed data and routes

--- app.js
+++ app.js
@@ -1,8 +1,72 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const data = require('./data.js')
+app.use(morgan('dev'))
 
-app.get("/" , (req,res,next) => {
-    res.send('hello')
+
+app.get("/" , (req, res, next) => {
+    const breweries = data.list()
+    const html = 
+    `<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Breweries</title>
+        </head>
+        <body>
+        <h1>Breweries</h1>
+        <div>
+        <ul>
+            ${breweries.map(brewery => `
+            <li><a href = "/breweries/${brewery.id}">${brewery.name}</li></a>
+                
+            
+            `).join('')}
+        </ul>
+        
+        </div>
+        </body>
+    </html>
+    
+    `
+    res.send(html)
+})
+
+app.get("/breweries/:id", (req,res,next)=>{
+    const brewery = data.find(req.params.id)
+    if(!brewery) {
+        return next(new Error('page not found'))
+    }    
+    //const breweries = [ breweryId ]
+    //console.log(breweries)
+    const html = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Breweries</title>
+        </head>
+        <body>
+        <a href="/">Go Back and Explore</a>
+        <h1>Brewery Info</h1>
+        <div>
+        
+        <i class="fa-light fa-beer-mug"></i>
+        <h3>${brewery.name}</h3>
+       
+
+            <div>
+            <h5>${brewery.brewery_type}</h5>
+            <h5>${brewery.street}</h5>
+            <p>${brewery.about}</p>
+            </div>
+       
+        </div>
+        </body>
+    </html>
+    `
+    res.send(html)
+    
+
 })
 
 const port = 3000
